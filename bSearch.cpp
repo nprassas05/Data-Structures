2d64d3ced76c81638becc83bdd28dfecc6c7dc6e
nprassas05 <nprassas01@manhattan.edu>
2016-12-21 17:40:43 -0500

    Add files via upload

---
+++ bSearch.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int recursiveBinarySearch(vector<int>&, int, int, int);
+
+int main()
+{
+    vector<int> vec;
+    vec.push_back(2);
+    vec.push_back(5);
+    vec.push_back(12);
+    vec.push_back(18);
+    vec.push_back(21);
+    vec.push_back(25);
+    vec.push_back(30);
+
+    int searchNum;
+    cout << "Enter a number to search for (Enter -9999 to quit): ";
+    cin >> searchNum;
+
+    while(searchNum != -9999) {
+        int result = recursiveBinarySearch(
+            vec, 0, vec.size() - 1, searchNum);
+        if (result == -1)
+            cout << "That number is not present" << endl;
+        else {
+            cout << "That nuber is located at ";
+            cout << "index " << result << endl;
+        }
+
+    cout << "Enter a number to search for (Enter -9999 to quit): ";
+    cin >> searchNum;
+
+    }
+
+    return 0;
+}
+
+
+
+int recursiveBinarySearch(vector<int>& v, int first, 
+            int last, int target) {
+    if (first > last)
+        return -1;
+    
+    int middleIndex = (first + last) / 2;
+    int middleElement = v[middleIndex];
+    
+    if (middleElement == target)
+        return middleIndex;
+    else if (middleElement > target)
+        return recursiveBinarySearch(v, first, middleIndex - 1, target);
+    else if (middleElement < target)
+        return recursiveBinarySearch(v, middleIndex + 1, last, target);
+}
